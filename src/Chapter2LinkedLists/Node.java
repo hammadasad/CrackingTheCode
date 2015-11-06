8cd1b0650d1d96df3ffe1f3e9703c93c64e1c3aa
Hammad Asad <hammad_asad@msn.com>
2015-11-06 17:13:51 -0500

    Implemented Linked List Problems

--- src/Chapter2LinkedLists/Node.java
+++ src/Chapter2LinkedLists/Node.java
@@ -46,6 +46,54 @@ public class Node {
 
     }
 
+    //Problem 2.1
+    //Write code to remove duplicates from an unsorted linked list. No Buffer
+    public static void removeDuplicates(Node head) {
+        for(Node current = head; current != null; current = current.next) {
+            for(Node compared = current; compared != null; compared = compared.next) {
+                if(compared.next != null) {
+                    if(current.data == compared.next.data) {
+                        compared.next = compared.next.next;
+                    }
+                }
+            }
+        }
+    }
+
+    //Problem 2.2
+    //Implement an algorithm to find the nth to last element of a singly linked list.
+    public static Node linkedListSubstring(Node head, int num) {
+        if(num == 1) {
+            return head;
+        }
+        int counter = 2;
+        for(Node current = head.next; current != null; current = current.next) {
+            if(counter == num) {
+                return current;
+            }
+            counter++;
+        }
+        return null;
+    }
+
+    //Problem 2.3
+    //Implement an algorithm to delete a node in the middle of a single linked list, given only access to that node.
+    //EXAMPLE
+    //Input: the node ‘c’ from the linked list a->b->c->d->e
+    //Result: nothing is returned, but the new linked list looks like a->b->d->e
+    public static void removeMiddleNode(Node notHead) {
+        Node current = notHead;
+        while(current != null) {
+            if(current.next != null) {
+                current.data = current.next.data;
+            }
+            if(current.next.next == null) {
+                current.next = null;
+            }
+            current = current.next;
+        }
+    }
+
     public static void main(String[] args) {
         Node list = new Node(1);
         list.appendNode(2);
@@ -55,6 +103,20 @@ public class Node {
         System.out.println("Delete Node 3");
         Node.deleteNode(list,3);
         Node.printList(list);
+        System.out.println("Adding a duplicate, which are 1,2,3");
+        list.appendNode(1);
+        list.appendNode(2);
+        list.appendNode(3);
+        Node.printList(list);
+        System.out.println("Removing duplicates");
+        Node.removeDuplicates(list);
+        Node.printList(list);
+        System.out.println("Linked List Substring starting 3rd element");
+        Node list2 = Node.linkedListSubstring(list, 3);
+        Node.printList(list2);
+        System.out.println("Linked List Substring starting 2nd element");
+        Node list3 = Node.linkedListSubstring(list, 2);
+        Node.printList(list3);
     }
 
 
